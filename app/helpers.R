f435575b8ef242ac02c3c731d5e9fabdb8681faf
Anderson Hiroshi Hamamoto <anderson@Andersons-MacBook-Pro.local>
2015-10-29 22:53:29 -0200

    app folder removed

--- app/helpers.R
+++
@@ -1,9 +0,0 @@
-# função que retorna o NMSE de duas séries temporais
-nmse <- function(real_traffic, dsnsf) {
-    value <- numeric()
-    for (i in 1:length(dsnsf)) {
-        value <- mean((dsnsf[i] - real_traffic[i]) ^ 2) /
-                 (mean(dsnsf[i]) * mean(real_traffic[i]))
-    }
-    return(value)
-}
